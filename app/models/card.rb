c4bdd53349f6cfc8f235ea2a206b2dc3e09f604e
Austin Aldrich <aualdrich@gmail.com>
2026-03-02 09:00:25 -0800

    feat: add active/archived scopes and archive!/restore! to Card model

--- app/models/card.rb
+++ app/models/card.rb
@@ -5,4 +5,15 @@ class Card < ApplicationRecord
   validates :project_id, presence: true
 
   default_scope { order(position: :asc) }
+
+  scope :active, -> { where(archived: false) }
+  scope :archived, -> { where(archived: true) }
+
+  def archive!
+    update!(archived: true, archived_at: Time.current)
+  end
+
+  def restore!
+    update!(archived: false, archived_at: nil)
+  end
 end
